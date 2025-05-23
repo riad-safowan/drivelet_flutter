a1329987d040afbb01f2044e3491287e0940f22e
Riad Safowan <riad.safowan@northsouth.edu>
2025-05-23 10:41:37 +0600

    NDK update.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.riadsafowan.drivelet.drivelet"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456-rc1"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
